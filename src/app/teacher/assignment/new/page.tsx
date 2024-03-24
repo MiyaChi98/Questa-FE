ae431d3e5c391277967d27f524730b56167fa27b
chinttmor <149034455+chinttmor@users.noreply.github.com>
2024-03-24 21:21:50 +0700

    add new assignment

--- src/app/teacher/assignment/new/page.tsx
+++ src/app/teacher/assignment/new/page.tsx
@@ -1,8 +1,25 @@
 'use client';
 const New_Assignment = () => {
   return (
-    <div className="mt-10 content-end">
-        New assignment
+    <div className="h-[83vh] mt-10 grid grid-cols-7 gap-4">
+      <div className="col-span-2 rounded-xl bg-white p-2 backdrop-blur-xl ">
+        <div className="m-3 text-xl font-bold text-indigo-900">
+          Assignment Infomation
+        </div>
+        <hr />
+        <div className="m-7">
+
+        </div>
+        </div>
+      <div className="col-span-5 rounded-xl bg-white p-2 backdrop-blur-xl ">
+      <div className="m-3 text-xl font-bold text-indigo-900">
+          Add Question
+        </div>
+        <hr />
+        <div className="m-7">
+          
+        </div>
+        </div>
     </div>
   );
 };
